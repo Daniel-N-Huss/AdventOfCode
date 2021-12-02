60952bbdedc601f0b891f9dda645862689ff1ede
Daniel Huss <63384617+Daniel-N-Huss@users.noreply.github.com>
2021-12-02 07:31:49 -0700

    handle instruction interpretations, output result for day 1 challenge

--- solutions/d2.rb
+++ solutions/d2.rb
@@ -1,13 +1,37 @@
 class Dive
   attr_reader :clean
+  attr_accessor :position
   def initialize(test_input = nil)
-    @input = test_input || File.open("./inputs/d2.txt").read
+    @input = test_input || File.open("../inputs/d2.txt").read
     @clean = cleanup
-
+    @position = { depth: 0, horizontal: 0 }
   end
 
   def cleanup
     @input.split("\n").map(&:split)
   end
 
+  def evaluate_directions
+    clean.each do |instruction|
+      if instruction.first == "forward"
+        position[:horizontal] += instruction.last.to_i
+      end
+
+      if instruction.first == "down"
+        position[:depth] += instruction.last.to_i
+      end
+
+      if instruction.first == "up"
+        position[:depth] -= instruction.last.to_i
+      end
+    end
+  end
+
+  def result
+    position[:depth] * position[:horizontal]
+  end
 end
+
+d = Dive.new
+d.evaluate_directions
+puts d.result
--- spec/dive_spec.rb
+++ spec/dive_spec.rb
@@ -2,15 +2,83 @@ require 'rspec'
 require_relative '../solutions/d2'
 
 RSpec.describe Dive do
-  describe 'initialize' do
-    subject { described_class.new }
+  subject { described_class.new(test_data) }
+  let(:test_data) { nil }
 
-    it 'reads the input file' do
-      puts subject.clean.inspect
-      expect(subject.clean.count).to be > 1
+  describe 'initialize' do
+    context 'when no test data is provided' do
+
+      it 'reads the input file, when no test data is attached' do
+        expect(subject.clean.count).to be > 100
+      end
+    end
+
+    context 'when test data is provided' do
+      let(:test_data) { "Blah 2\nFake 1" }
+
+      it 'reads the test data' do
+        expect(subject.clean.count).to be 2
+        expect(subject.clean.first).to eq ["Blah", "2"]
+      end
+    end
+
+    it 'has a position measurement' do
+      expect(subject.position).to eq({depth: 0, horizontal: 0})
     end
   end
 
-  describe '' do
+  describe '.evaluate_directions' do
+    let(:dive) { Dive.new(test_data) }
+    subject { dive.evaluate_directions }
+
+    context 'when instructions include "forward"' do
+      let(:test_data) { "forward 2" }
+
+      it "increments the horizontal position" do
+        subject
+        expect(dive.position).to eq({ depth: 0, horizontal: 2 })
+      end
+    end
+
+    context 'when instructions include "down"' do
+      let(:test_data) { "down 2" }
+
+      it "increments the depth position" do
+        subject
+        expect(dive.position).to eq({ depth: 2, horizontal: 0 })
+      end
+    end
+
+    context 'when instructions include "up"' do
+      let(:test_data) { "up 2" }
+
+      it "decrements the depth position" do
+        subject
+        expect(dive.position).to eq({ depth: -2, horizontal: 0 })
+      end
+    end
+
+    context 'when instructions include all directions' do
+      let(:test_data) { "down 4\nup 2\nforward 3" }
+
+      it "decrements the depth position" do
+        subject
+        expect(dive.position).to eq({ depth: 2, horizontal: 3 })
+      end
+
+    end
+  end
+
+  describe '.result' do
+    let(:dive) { described_class.new(test_data) }
+    subject { dive.result }
+    let(:test_data) { "placeholder" }
+
+    before do
+      dive.position = { depth: 2, horizontal: 2 }
+    end
+    it 'returns the multiplication of both positional elements' do
+      expect(subject).to eq 4
+    end
   end
 end
