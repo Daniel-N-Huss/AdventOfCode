9573d8db7188ad5a2d5e4fc794d6deb64e0088b9
Daniel Huss <63384617+Daniel-N-Huss@users.noreply.github.com>
2021-12-03 07:57:37 -0700

    add specs for calculating bit rates, implement helper function

--- solutions/d3.rb
+++ solutions/d3.rb
@@ -7,4 +7,14 @@ class PowerCalculator
     @clean = @input.split("\n")
     @rate_collection = { '0': [], "1": [], "2": [], '3': [], '4': [], '5': [], '6': [], '7': [], '8': [], '9': [], '10': [], '11': [] }
   end
+
+  def input_into_rate_collection
+    clean.each do |measurement|
+      measurement.split('').each_with_index { |bit, index| rate_collection[index.to_s.to_sym] << bit.to_i }
+    end
+  end
+
+  def most_common_bit(bits)
+    bits.max_by{ |bit| bits.count(bit) }
+  end
 end
--- spec/power_calculator_spec.rb
+++ spec/power_calculator_spec.rb
@@ -27,4 +27,48 @@ RSpec.describe PowerCalculator do
     end
   end
 
+  describe ".input_into_rate_collection" do
+    let(:power_calculator) { described_class.new(test_data) }
+    let(:test_data) { "110000000001\n010011111011" }
+
+    subject { power_calculator.input_into_rate_collection }
+
+    it 'splits the input into the indexed rate_collection' do
+      subject
+      expect(power_calculator.rate_collection).to eq({ '0': [1, 0], "1": [1, 1], "2": [0, 0], '3': [0, 0], '4': [0, 1], '5': [0, 1], '6': [0, 1], '7': [0, 1], '8': [0, 1], '9': [0, 0], '10': [0, 1], '11': [1, 1] })
+
+    end
+  end
+
+  describe 'most_common_bit' do
+    subject { described_class.new.most_common_bit(rate_count) }
+
+    context 'with majority 0 bits' do
+      let(:rate_count) { [1, 0, 0] }
+
+      it { is_expected.to eq 0 }
+    end
+
+    context 'with majority 1 bits' do
+      let(:rate_count) { [1, 1, 0] }
+
+      it { is_expected.to eq 1 }
+    end
+
+  end
+
+  describe 'reduce_rate_collection' do
+    let(:power_calculator) { described_class.new(test_data) }
+    let(:test_data) { "placeholder" }
+
+    subject { power_calculator.reduce_rate_collection }
+
+    before do
+      power_calculator.rate_collection = { '0': [1, 0, 0], "1": [1, 1, 1], "2": [0, 0, 1], '3': [0, 0, 0], '4': [0, 1, 1], '5': [0, 1, 0], '6': [0, 1, 1], '7': [0, 1, 0], '8': [0, 1, 1], '9': [0, 0, 0], '10': [0, 1, 1], '11': [1, 1, 1] }
+    end
+
+    it 'returns the most common bits across all measurements' do
+      expect(subject).to eq("010010101011")
+    end
+  end
 end
