d30b80f7a5784c2a82d8075f1dfef49c7409ada0
Daniel Huss <63384617+Daniel-N-Huss@users.noreply.github.com>
2021-12-03 06:59:45 -0700

    tweaks based on input data length

--- solutions/d3.rb
+++ solutions/d3.rb
@@ -1,3 +1,10 @@
 class PowerCalculator
+  attr_reader :clean
+  attr_accessor :rate_collection
 
+  def initialize(test_data = nil)
+    @input = test_data || File.open("./inputs/d3.txt").read
+    @clean = @input.split("\n")
+    @rate_collection = { '0': [], "1": [], "2": [], '3': [], '4': [], '5': [], '6': [], '7': [], '8': [], '9': [], '10': [], '11': [] }
+  end
 end
--- spec/power_calculator_spec.rb
+++ spec/power_calculator_spec.rb
@@ -14,16 +14,16 @@ RSpec.describe PowerCalculator do
     end
 
     context 'when test data is provided' do
-      let(:test_data) { "10010\n00110" }
+      let(:test_data) { "110000000001\n010011111011" }
 
       it 'reads the test data' do
         expect(subject.clean.count).to be 2
-        expect(subject.clean.first).to eq [10010, 00110]
+        expect(subject.clean).to eq ['110000000001', '010011111011']
       end
     end
 
     it 'has a rate_collection measurement' do
-      expect(subject.rate_collection).to eq({ '0': [], "1": [], "2": [], '3': [], '4': [] })
+      expect(subject.rate_collection).to eq({ '0': [], "1": [], "2": [], '3': [], '4': [], '5': [], '6': [], '7': [], '8': [], '9': [], '10': [], '11': [] })
     end
   end
 
