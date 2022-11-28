6f1ee9a7f29d40cf76a27b8afe705720452335b8
Daniel Huss <63384617+Daniel-N-Huss@users.noreply.github.com>
2022-11-28 13:20:28 -0700

    2021 d4 stuff

--- 2021/solutions/d4.rb
+++ 2021/solutions/d4.rb
@@ -1,8 +1,9 @@
 class Bingo
   attr_reader :input
   attr_accessor :call_numbers, :game_boards, :raw_board_data
+
   def initialize(bingo_games = nil)
-    @input = bingo_games || File.open("./inputs/d4.txt").read
+    @input = bingo_games || File.open(File.absolute_path("../inputs/d4.txt")).read
     @parsed_games = parse_games
     @call_numbers = @parsed_games[:call_numbers]
     @raw_board_data = @parsed_games[:raw_boards]
@@ -14,11 +15,11 @@ class Bingo
 
     split_call_from_boards = input.split("\n")
 
-    result[:call_numbers] = split_call_from_boards.shift.split(",")
+    result[:call_numbers] = split_call_from_boards.shift.split(',')
 
-    board_rows = split_call_from_boards.select { |row| !row.empty? }
+    board_rows = split_call_from_boards.reject(&:empty?)
     clean_board_rows = board_rows.collect(&:strip)
-    result[:raw_boards] = clean_board_rows.join(" ")
+    result[:raw_boards] = clean_board_rows.join(' ')
 
     result[:boards] = []
     (clean_board_rows.count / 5).times do
@@ -27,11 +28,96 @@ class Bingo
 
     result
   end
+
+  def play
+    call_numbers.each do |calling|
+
+      if @game_boards.count == 1
+        last_board = @game_boards.first
+        last_board.call(calling)
+        result = last_board.tally * calling.to_i
+        puts result
+        return result
+      end
+
+      @game_boards.each do |board|
+        board.call(calling)
+
+        if board.matched_count >= 5
+          if board.wins?
+            @game_boards = @game_boards.reject { |board| board.won == true }
+          end
+        end
+      end
+    end
+  end
 end
 
 class Board
-  attr_reader :rows
-  def initialize(rows)
+  attr_reader :rows, :top, :upper, :middle, :lower, :bottom, :row_list, :matched_count, :won
+
+  def initialize(rows, overrides = {})
     @rows = rows
+    @top = overrides[:top] || parse_row(rows.first)
+    @upper = overrides[:upper] || parse_row(rows[1])
+    @middle = overrides[:middle] || parse_row(rows[2])
+    @lower = overrides[:lower] || parse_row(rows[3])
+    @bottom = overrides[:bottom] || parse_row(rows[4])
+    @row_list = [@top, @upper, @middle, @lower, @bottom]
+    @matched_count = 0
+    @won = false
+  end
+
+  def parse_row(row)
+    row.split(' ').collect(&:to_i)
+  end
+
+  def call(number)
+    match_found = false
+
+    row_list.each do |row|
+      if replace_matched_number(number, row)
+        match_found = true
+        @matched_count += 1
+        break
+      end
+    end
+
+    match_found
+  end
+
+  def wins?
+    row_list.each do |row|
+      if row == [true, true, true, true, true]
+        @won = true
+        return true
+      end
+    end
+
+    possible_column_wins = []
+
+    @top.each_with_index { |value, index| possible_column_wins << index if value == true }
+
+    possible_column_wins.each do |column_index|
+      if (@upper[column_index] == true && @middle[column_index] == true && @lower[column_index] == true && @bottom[column_index] == true)
+        @won = true
+        return true
+      end
+    end
+
+    false
+  end
+
+  def tally
+    @row_list.collect do |row|
+      row.reject { |number| number == true }.sum
+    end.sum
+  end
+
+  def replace_matched_number(number, row)
+    number_location = row.index(number.to_i)
+    row[number_location] = true if number_location
   end
 end
+
+Bingo.new.play
--- 2021/spec/bingo_spec.rb
+++ 2021/spec/bingo_spec.rb
@@ -2,6 +2,28 @@ require 'rspec'
 require_relative '../solutions/d4'
 
 RSpec.describe Bingo do
+  let(:bingo_games) do
+    "7,4,9,5,11,17,23,2,0,14,21,24,10,16,13,6,15,25,12,22,18,20,8,19,3,26,1
+
+22 13 17 11  0
+ 8  2 23  4 24
+21  9 14 16  7
+ 6 10  3 18  5
+ 1 12 20 15 19
+
+ 3 15  0  2 22
+ 9 18 13 17  5
+19  8  7 25 23
+20 11 10 24  4
+14 21 16 12  6
+
+14 21 17 24  4
+10 16 15  9 19
+18  8 23 26 20
+22 11 13  6  5
+ 2  0 12  3  7"
+  end
+
   describe 'initialize' do
     subject { described_class.new(bingo_games) }
 
@@ -14,53 +36,197 @@ RSpec.describe Bingo do
     end
 
     context 'when test data is provided' do
-      let(:bingo_games) do
-        "10,80,6,69,22,99
-
-      3 82 18 50 90
-      16 37 52 67 28
-      30 54 80 11 10
-      60 79  7 65 58
-      76 83 38 51  1
-
-      10 80 6 69 99
-      70 87  5 22 14
-      85  3 11 16 33
-      72 69 97 36 49
-      26 17 58 13  2"
-      end
-
-      let(:raw_boards) do
-        "3 82 18 50 90 16 37 52 67 28 30 54 80 11 10 60 79  7 65 58 76 83 38 51  1 10 80 6 69 99 70 87  5 22 14 85  3 11 16 33 72 69 97 36 49 26 17 58 13  2"
-      end
-
       it 'reads the bingo_games' do
         expect(subject.input).to eq bingo_games
       end
 
       it 'has a list of call_numbers' do
-        expect(subject.call_numbers).to eq(['10', '80', '6', '69', '22', '99'])
+        expect(subject.call_numbers).to eq %w[7 4 9 5 11 17 23 2 0 14 21 24 10 16 13 6 15 25 12 22 18 20 8 19 3 26 1]
       end
 
       it 'has a collection of game boards' do
-        expect(subject.game_boards.count).to eq 2
+        expect(subject.game_boards.count).to eq 3
       end
 
       it 'holds the raw board data' do
-        expect(subject.raw_board_data).to eq raw_boards
+        expect(subject.raw_board_data).to include('22 13 17 11  0')
+        expect(subject.raw_board_data).to include('8  2 23  4 24')
+        expect(subject.raw_board_data).to include('21  9 14 16  7')
+        expect(subject.raw_board_data).to include('6 10  3 18  5')
+        expect(subject.raw_board_data).to include('1 12 20 15 19')
+
+        expect(subject.raw_board_data).not_to include('7 4 9 5 11 17 23 2 0 14 21 24 10 16 13 6 15 25 12 22 18 20 8 19 3 26 1')
+
       end
     end
   end
 
+  # split out the list of call numbers, and game boards
+  # draw one number from the list at a time
+  # mark each number as drawn on all boards
+  # check if the board has a completed row
+  # score the board by
+  #   sum all unmarked numbers
+  #   multiply sum by the number that was called
+
+  describe ".play" do
+    subject { bingo.play }
+
+    let(:bingo) { Bingo.new(bingo_games) }
+
+    it { is_expected.to eq 1924 }
+  end
 end
 
 RSpec.describe Board do
-  describe 'initialize' do
-    subject { described_class.new(test_data) }
+  let(:board_numbers) do
+    ['22 13 17 11  0',
+     '8  2 23  4 24',
+     '21  9 14 16  7',
+     '6 10  3 18  5',
+     '1 12 20 15 19']
+  end
 
-    let(:test_data) { ["3 82 18 50 90", "16 37 52 67 28", "30 54 80 11 10", "60 79  7 65 58", "76 83 38 51  1"] }
-    it 'has rows' do
-      expect(subject.rows).to eq test_data
+  describe 'initialize' do
+    subject { Board.new(board_numbers) }
+
+    it "tracks numbers in each row" do
+      expect(subject.top).to eq [22, 13, 17, 11, 0]
+      expect(subject.upper).to eq [8, 2, 23, 4, 24]
+      expect(subject.middle).to eq [21, 9, 14, 16, 7]
+      expect(subject.lower).to eq [6, 10, 3, 18, 5]
+      expect(subject.bottom).to eq [1, 12, 20, 15, 19]
     end
+
+    it "tracks matched count" do
+      expect(subject.matched_count).to eq 0
+    end
+
+    it "knows if it's won already" do
+      expect(subject.won).to eq false
+    end
+  end
+
+  describe ".call" do
+    subject { board.call(number) }
+
+    let(:board) { Board.new(board_numbers) }
+
+    context "when called number does not exist on the board" do
+      let(:number) { '99' }
+
+      it "does nothing" do
+        expect { subject }.not_to change { board.top }
+        expect { subject }.not_to change { board.upper }
+        expect { subject }.not_to change { board.middle }
+        expect { subject }.not_to change { board.lower }
+        expect { subject }.not_to change { board.bottom }
+
+        expect(subject).to eq false
+      end
+    end
+
+    context "when called number exists on the board" do
+      let(:number) { '22' }
+
+      it "changes the called number to a boolean" do
+        expect(subject).to eq true
+        expect(board.top.first).to eq true
+      end
+
+      it "increments the matched count" do
+        expect { subject }.to change { board.matched_count }.by 1
+      end
+    end
+  end
+
+  describe ".wins?" do
+    subject { board.wins? }
+    context "when board does not have matched numbers" do
+      let(:board) { Board.new(board_numbers) }
+
+      it { is_expected.to be false }
+    end
+
+    context "when board has matched numbers" do
+      let(:board) { Board.new(board_numbers, test_override) }
+
+      context "when there are matches, but no full row or column" do
+        let(:test_override) do
+          { top: [true, 13, 17, 11, 0],
+            upper: [8, 2, true, 4, 24],
+            middle: [21, 9, 14, 16, 7],
+            lower: [6, 10, 3, 18, true],
+            bottom: [1, 12, 20, true, 19]
+          }
+        end
+
+        it { is_expected.to be false }
+      end
+
+      context "when a full column matches" do
+        let(:test_override) do
+          { top: [true, 13, 17, 11, 0],
+            upper: [true, 2, 11, 4, 24],
+            middle: [true, 9, 14, 16, 7],
+            lower: [true, 10, 3, 18, 44],
+            bottom: [true, 12, 20, 13, 19]
+          }
+        end
+
+        it { is_expected.to be true }
+
+        it "updates the won tracking" do
+          subject
+          expect(board.won).to eq true
+        end
+      end
+
+      context "when a full column matches, with extra true values" do
+        let(:test_override) do
+          { top: [true, 13, true, 11, true],
+            upper: [true, 2, 11, true, 24],
+            middle: [true, 9, 14, 16, 7],
+            lower: [true, 10, 3, 18, 44],
+            bottom: [true, 12, 20, 13, 19]
+          }
+        end
+
+        it { is_expected.to be true }
+      end
+
+      context "when a full row matches" do
+        let(:test_override) do
+          { top: [8, 13, 17, 11, 0],
+            upper: [21, 2, 11, 4, 24],
+            middle: [6, 9, 14, 16, 7],
+            lower: [1, 10, 3, 18, 44],
+            bottom: [true, true, true, true, true]
+          }
+        end
+
+        it { is_expected.to be true }
+      end
+    end
+  end
+
+
+  describe ".tally" do
+    subject { board.tally }
+
+    let(:board) { Board.new(board_numbers, test_override) }
+
+    let(:test_override) do
+      { top: [true, 13, true, 11, true],
+        upper: [true, 2, 11, true, 24],
+        middle: [true, 9, 14, 16, 7],
+        lower: [true, 10, 3, 18, 44],
+        bottom: [true, 12, 20, 13, 19]
+      }
+    end
+
+    let(:non_marked_numbers) { [13, 11, 2, 11, 24, 9, 14, 16, 7, 10, 3, 18, 44, 12, 20, 13, 19] }
+
+    it { is_expected.to eq non_marked_numbers.sum }
   end
 end
