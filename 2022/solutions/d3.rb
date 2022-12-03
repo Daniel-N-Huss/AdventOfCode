9e7b21b374a5ec9f7733b2550dc888680fde9ca5
Daniel Huss <63384617+Daniel-N-Huss@users.noreply.github.com>
2022-12-03 13:08:52 -0700

    lean on each slice for divisions

--- 2022/solutions/d3.rb
+++ 2022/solutions/d3.rb
@@ -8,32 +8,22 @@ class RucksackPacker
 
   def priority_of_mispacked_items
     @packed_rucksacks.collect do |pack|
-      midpoint = (pack.length / 2)
-      first_compartment = pack[0..(midpoint) - 1]
-
-      second_compartment = pack[midpoint..-1]
-
-      mistakenly_packed_item = first_compartment.chars.intersection(second_compartment.chars).first
+      pack_compartments = pack.chars.each_slice(pack.length / 2).to_a
+      mistakenly_packed_item = pack_compartments.first.intersection(pack_compartments.last).first
 
       PRIORITY.find_index(mistakenly_packed_item)
     end.sum
   end
 
   def badge_item_priority
-    groups = @packed_rucksacks.each_slice(3).to_a
-
-    badges = groups.collect do |group|
-      first_pack = group.first.chars
-      second_pack = group[1].chars
-      third_pack = group.last.chars
-
-      first_pack.intersection(second_pack, third_pack).first
+    badges = []
+    @packed_rucksacks.each_slice(3) do |first_pack, second_pack, third_pack|
+      badges << first_pack.chars.intersection(second_pack.chars, third_pack.chars).first
     end
 
     badges.collect { |badge| PRIORITY.find_index(badge) }.sum
   end
 end
-
 
 # packing_list = File.open(File.absolute_path("./2022/inputs/d3.txt")).read
 
