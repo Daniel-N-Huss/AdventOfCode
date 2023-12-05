1d033f35fcb219fa9108127b0cde67277fe6117e
Daniel Huss <63384617+Daniel-N-Huss@users.noreply.github.com>
2023-12-05 11:41:49 -0700

    some refactors

--- 2023/solutions/d4.rb
+++ 2023/solutions/d4.rb
@@ -4,24 +4,17 @@ class ScratchCardPointCounter
   end
 
   def find_winning_numbers
-    scratch_games = @ticket_pile.split("\n").map {|game| game.split(": ").last }
+    scratch_games = @ticket_pile.split("\n").map { |game| game.split(": ").last }
 
-    ticket_numbers = scratch_games.map { |nums| nums.split("|").map { |numbers| numbers.split(" ").map(&:to_i) }}
+    ticket_numbers = scratch_games.map { |nums| nums.split("|").map { |numbers| numbers.split(" ").map(&:to_i) } }
 
-    winning_numbers = ticket_numbers.map { |game| game.first & game.last }
-
-    winning_numbers
+    ticket_numbers.map { |game| game.first & game.last }
   end
 
   def score_winning_numbers
-    winning_numbers = find_winning_numbers
-
-    winning_numbers.map do |ticket|
-      case
-      when ticket.length < 2
+    find_winning_numbers.map do |ticket|
+      if ticket.length <= 2
         ticket.length
-      when ticket.length == 2
-        2
       else
         ticket.pop
         ticket.reduce(1) { |memo, _num| memo * 2 }
@@ -30,13 +23,11 @@ class ScratchCardPointCounter
   end
 
   def setup_ticket_library
-    winning_numbers = find_winning_numbers
-
     @ticket_library ||= {}
 
-    winning_numbers.each_with_index do |ticket, index|
-      @ticket_library[(index + 1)] = { winning_numbers: ticket.length, play_count: 1 }
-    end if @ticket_library.empty?
+    find_winning_numbers.each_with_index do |ticket, index|
+      @ticket_library[index + 1] = { winning_numbers: ticket.length, play_count: 1 }
+    end
 
     @ticket_library
   end
@@ -45,15 +36,11 @@ class ScratchCardPointCounter
     setup_ticket_library
 
     @ticket_library.each do |game_number, data|
-      case
-      when data[:winning_numbers] <= 0
-        next
-      when data[:winning_numbers] == 1
-        @ticket_library[(game_number + 1)][:play_count] += data[:play_count] if @ticket_library[(game_number + 1)]
-      else
-        ((game_number + 1)..(game_number + data[:winning_numbers])).each do |ticket_number|
-          @ticket_library[ticket_number][:play_count] += data[:play_count] if @ticket_library[ticket_number]
-        end
+      next if data[:winning_numbers] <= 0
+
+      games_to_add_plays = ((game_number + 1)..(game_number + data[:winning_numbers]))
+      games_to_add_plays.each do |game|
+        add_plays_to_next_game(data, game)
       end
     end
 
@@ -63,5 +50,11 @@ class ScratchCardPointCounter
   def count_played_tickets
     play
     @ticket_library.values.map { |data| data[:play_count] }.sum
+  end
+
+  private
+
+  def add_plays_to_next_game(data, game_number)
+    @ticket_library[(game_number)][:play_count] += data[:play_count] if @ticket_library[(game_number)]
   end
 end
